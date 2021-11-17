4b0a6ced44438999b7b6b755f1d0b4191d6d3e88
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-11-16 22:11:04 -0500

    Updated version and changelog

--- Hire/AssemblyVersion.cs
+++ Hire/AssemblyVersion.cs
@@ -10,5 +10,5 @@
   
  using System.Reflection;
 
- [assembly: AssemblyVersion("0.6.10.3")]
- [assembly: AssemblyFileVersion("0.6.10.3")]
+ [assembly: AssemblyVersion("0.6.11.0")]
+ [assembly: AssemblyFileVersion("0.6.11.0")]
